1e146a206c458f64437a84c243ed6234ea098240
CyberGA <etomu.joshua@gmail.com>
2021-04-19 03:34:02 +0100

    Finished the task

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const fetch = require("node-fetch");
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const loadPost = async (url) => {
+  const response = await fetch(url);
+  if (response.status === 200) {
+    const posts = await response.json();
+
+    fs.mkdir("result", (err) => {
+      if (err) throw err;
+      if (!err) {
+        fs.writeFile(
+          "./result/posts.json",
+          JSON.stringify(posts, null, 2),
+          (err) => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      }
+    });
+  }
+};
+loadPost(url);
